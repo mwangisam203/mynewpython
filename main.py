44585724b78f6dc21e88a1711c5bd5132fdd3a8a
mwangisam203 <mwangisam203@gmail.com>
2025-12-22 16:28:39 -0500

    test cases

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 fruits = ("apple", "banana", "cherry", "date", "elderberry")
 # Accessing elements by index
 print(fruits[-1:-3]) 
+print(fruits[-3:-1])#output: ('cherry', 'date')
+print(fruits[1:4])  #output: ('banana', 'cherry',
+print(fruits[::2])  #output: ('apple', 'cherry', 'elderberry') 'date')
+print(fruits[::-1]) #output: ('elderberry', 'date', 'cherry', 'banana', 'apple')
+print(fruits[::-2])  #output: ('apple', 'cherry', 'elderberry')
+
+
+sweets = ("apple", "banana", "cherry", "elderberry")
+print(sweets[2:-1]) #output: ('cherry',)
+print(sweets[2::-1])#output: ('cherry', 'banana', 'apple')
+print(sweets[-1:-4]) #output: () 
