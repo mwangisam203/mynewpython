54f4630e59f75beab055c2acd90b3fabc194d807
mwangisam203 <mwangisam203@gmail.com>
2025-12-29 08:18:32 -0500

    modified

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 fruits = ("apple", "banana", "cherry", "date", "elderberry")
 # Accessing elements by index
-print(fruits[-1:-3]) 
+print(fruits[-1:-3]) #output: ()
 print(fruits[-3:-1])#output: ('cherry', 'date')
 print(fruits[1:4])  #output: ('banana', 'cherry',
 print(fruits[::2])  #output: ('apple', 'cherry', 'elderberry') 'date')
